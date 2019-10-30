a0b5d144ec56cb745d38e7fce2bff5e396d05309
txp761 <txp761@student.bham.ac.uk>
2019-10-30 15:16:24 +0000

    fixed more valgrind errors

--- bst.c
+++ bst.c
@@ -1,101 +1,130 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "bst.h"
 
 // Your source code
 
-Node* insertNode(Node *root, int value){ 
-    // check for the empty tree. 
-    if(root == NULL){
-        root = (Node*) malloc(sizeof(Node));
-        if(root == NULL){
+Node *insertNode(Node *root, int value)
+{
+    // check for the empty tree.
+    if (root == NULL)
+    {
+        root = (Node *)malloc(sizeof(Node));
+        if (root == NULL)
+        {
             printf("Allocation unsuccessful\n");
             return NULL;
         }
-        // Initialise all data values. 
+        // Initialise all data values.
         root->data = value;
         root->left = NULL;
         root->right = NULL;
         printf("Node with value %d has been inserted\n", value);
-    }else if(value > root->data){ // Inserting the data. 
+    }
+    else if (value > root->data)
+    { // Inserting the data.
         printf("%d > %d so inserting right\n", value, root->data);
         root->right = insertNode(root->right, value);
-    }else{
+    }
+    else
+    {
         printf("%d < %d so inserting left\n", value, root->data);
         root->left = insertNode(root->left, value);
     }
     return root;
 }
 
-Node* deleteNode(Node *root, int value){
+Node *deleteNode(Node *root, int value)
+{
     // Firstly, we can check if the tree is empty...
-    if(root == NULL){
+    if (root == NULL)
+    {
         printf("Tree is empty!\n");
         return root;
     }
 
     // We first need to traverse to the part of the tree which holds our value...
-    if(value == root->data){
+    if (value == root->data)
+    {
         printf("%d = %d so deleting node\n", value, root->data);
-        if(root->left == NULL){ // Right children exist
+        if (root->left == NULL)
+        { // Right children exist
             printf("Deleting node\n");
-            Node* temp = root->right;
-            free(root);
-            return temp;
-        }else if(root->right == NULL){ // Left children exist. 
-            printf("Deleting node\n");
-            Node* temp = root->left;
+            Node *temp = root->right;
             free(root);
             return temp;
         }
-        // When left AND right children exist. 
-        Node* temp = findMinimum(root->right);
+        else if (root->right == NULL)
+        { // Left children exist.
+            printf("Deleting node\n");
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        // When left AND right children exist.
+        Node *temp = findMinimum(root->right);
         root->data = temp->data;
         root->right = deleteNode(root->right, temp->data);
-    }else if(value > root->data){
+    }
+    else if (value > root->data)
+    {
         printf("%d > %d so traversing right subtree\n", value, root->data);
         root->right = deleteNode(root->right, value);
-    }else{ // The default case where the value is less than the root. 
+    }
+    else
+    { // The default case where the value is less than the root.
         printf("%d < %d so traversing left subtree\n", value, root->data);
         root->left = deleteNode(root->left, value);
     }
     return root;
 }
 
-// inorder sucessor finds the minimum value of the right child of the root passed into it. 
-Node* findMinimum(Node* root){
-    while(root->left != NULL){
+// inorder sucessor finds the minimum value of the right child of the root passed into it.
+Node *findMinimum(Node *root)
+{
+    while (root->left != NULL)
+    {
         root = root->left;
     }
     return root;
 }
 
-// Prints using in order traversal. 
-void printSubtree(Node* N){
-    if(N == NULL){
+// Prints using in order traversal.
+void printSubtree(Node *N)
+{
+    if (N == NULL)
+    {
         return;
     }
-    
+
     printSubtree(N->left);
     printf("%d\n", N->data);
     printSubtree(N->right);
 }
 
-int countNodes(Node* N){
-    if(N == NULL){
+int countNodes(Node *N)
+{
+    printf("Inside count nodes\n");
+    if (N == NULL)
+    {
+        printf("Node is null!\n");
         return 0;
     }
     return 1 + countNodes(N->left) + countNodes(N->right);
 }
 
-Node* freeSubtree(Node* N){
-    // Start from bottom of the tree and go up. 
-    if(N == NULL){ // If tree is empty. 
+Node *freeSubtree(Node *N)
+{
+    // Start from bottom of the tree and go up.
+    if (N == NULL)
+    { // If tree is empty.
+        printf("Node is null\n");
         return N;
-    } 
-    freeSubtree(N->left);
-    freeSubtree(N->right);
+    }
+    N->left = freeSubtree(N->left);
+    N->right = freeSubtree(N->right);
     free(N);
+    N = NULL;
     printf("Node has been freed\n");
 
     return N;
--- test_bst.c
+++ test_bst.c
@@ -3,43 +3,43 @@
 #include <stdlib.h>
 int main()
 {
-	Node *root=NULL;
-	
+	Node *root = NULL;
+
 	printf("START OF INSERTION\n");
 	printf("==================\n");
-   	root=insertNode(root, 14);
+	root = insertNode(root, 14);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 12);
+	root = insertNode(root, 12);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 6);
+	root = insertNode(root, 6);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 17);
+	root = insertNode(root, 17);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 8);
+	root = insertNode(root, 8);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 24);
+	root = insertNode(root, 24);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 1);
+	root = insertNode(root, 1);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=insertNode(root, 99);
+	root = insertNode(root, 99);
 	printf("ROOT DATA IS %d\n\n", root->data);
 	printf("END OF INSERTION\n");
 	printf("==================\n\n");
-   	printSubtree(root);
+	printSubtree(root);
 	printf("\n");
 	printf("START OF DELETION\n");
 	printf("==================\n");
-	root=deleteNode(root,12);
+	root = deleteNode(root, 12);
 	printf("ROOT DATA IS %d\n\n", root->data);
-	root=deleteNode(root,8);
+	root = deleteNode(root, 8);
 	printf("ROOT DATA IS %d\n\n", root->data);
 	printf("END OF DELETION\n");
 	printf("==================\n");
-   	printSubtree(root);
+	printSubtree(root);
 	printf("Number of nodes=%d\n", countNodes(root));
 	printf("Running free subtree\n");
-	root=freeSubtree(root);
-
+	root = freeSubtree(root);
+	printf("Number of nodes=%d\n", countNodes(root));
 
 	return 0;
 }
